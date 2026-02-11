41292e0d2632207b3dc3c3e5a2d64dcf20ef9ab2
Roscoe Raynott <kohinoorrush@gmai.com>
2026-02-10 17:42:54 -0800

    feat: Add `run_all.m` script to execute `codehelp.m` and `LOOCVal.m` for Figure 6769.

---
+++ run_all.m
@@ -0,0 +1,6 @@
+%% Run codehelp.m then LOOCVal.m to generate figure 6769
+fprintf('=== Starting codehelp.m ===\n');
+codehelp;
+fprintf('\n=== codehelp.m finished. Starting LOOCVal.m ===\n');
+LOOCVal;
+fprintf('\n=== All done. Figure 6769 is ready. ===\n');
